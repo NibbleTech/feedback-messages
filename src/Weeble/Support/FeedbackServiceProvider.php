aa751c8123ddb9a5ff80ac84244725ee97db5696
Shaun Walker <shaunwalker@nibbletech.co.uk>
2014-03-15 18:07:10 +0000

    Add Laravel service provider

---
+++ src/Weeble/Support/FeedbackServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Weeble\Support;
+
+use Illuminate\Support\ServiceProvider;
+
+class FeedbackServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['feedback'] = $this->app->share(function($app)
+		{
+			// Return App::make not new Class because using App::make auto resolves dependencies.
+			return \App::make('Weeble\Support\FeedbackMessages');
+		});
+	}
+}
