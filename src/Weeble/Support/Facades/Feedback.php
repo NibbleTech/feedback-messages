bbed8740997c03b24e194ea34b2150f883cc28fa
Shaun Walker <shaunwalker@nibbletech.co.uk>
2014-03-15 18:26:45 +0000

    Add facade

---
+++ src/Weeble/Support/Facades/Feedback.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Gig2gig\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Feedback extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'feedback'; }
+}
