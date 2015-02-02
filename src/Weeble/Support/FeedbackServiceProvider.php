558d9a5ba4cdb4d54dc5058742c77ee8d832bb88
Shaun Walker <shaunwalker@nibbletech.co.uk>
2015-02-02 21:30:13 +0000

    Laravel 5 update

--- src/Weeble/Support/FeedbackServiceProvider.php
+++ src/Weeble/Support/FeedbackServiceProvider.php
@@ -13,14 +13,17 @@ class FeedbackServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
+		$this->mergeConfigFrom(
+			__DIR__ . "/../../config/config.php",
+			'weeble/support/feedback'
+		);
+
 		$this->app->bind('Weeble\Support\SessionHandlers\SessionHandlerInterface', 'Weeble\Support\SessionHandlers\LaravelSessionHandler');
 		$this->app->singleton('view.feedback', 'Weeble\Support\Feedback');
 	}
 
 	public function boot()
 	{
-		$this->package('weeble/support/feedback', 'weeble/support/feedback', __DIR__ . '/../../');
-
 		$this->app['view.feedback']->regenerateSession();
 		$this->app['view.feedback']->setTypeAlias($this->app['config']['weeble/support/feedback::types']);
 	}
